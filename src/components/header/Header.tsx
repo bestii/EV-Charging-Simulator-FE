1386cfb22123c70ec680fe02697edc7734eeb242
Bestin John <bestinjohn1993@gmail.com>
2025-03-23 03:40:10 +0100

    Add company logo to Header component and remove title text

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,10 @@
+import { default as logoSrc } from '../../assets/logo.png';
+
 const Header = () => {
   return (
     <header className="sticky top-0 border-b border-gray-950/5 bg-white px-4 py-4">
       <div className="container mx-auto">
-        <span className="text-xl font-bold text-gray-800">
-          EV Charging Simulator
-        </span>
+        <img className="w-[85px]" src={logoSrc} alt="company logo" />
       </div>
     </header>
   );
