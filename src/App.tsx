ab37e359d3cdd78935f21b155e0ad01683bd637d
Bestin John <bestinjohn1993@gmail.com>
2025-03-23 17:53:42 +0100

    Enhance App component with improved layout and descriptive header for better user engagement

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,18 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Header />
-      <main className="main p-4">
-        <div className="container mx-auto grid flex-1 grid-cols-1 gap-4 md:grid-cols-12">
+      <main className="main container mx-auto p-4">
+        <div className="my-10 text-center">
+          <h1 className="my-8 text-5xl tracking-tighter">
+            Plan Your EV Charging Setup Smartly
+          </h1>
+          <p className="mx-auto max-w-3xl">
+            Simulate energy consumption, peak power demand, and charging events
+            to optimize your charge point installation. Adjust parameters and
+            visualize the impact effortlessly. 🚗⚡📊
+          </p>
+        </div>
+        <div className="grid flex-1 grid-cols-1 gap-4 md:grid-cols-12">
           <ConfigForm />
           <SimuationPlots />
         </div>
