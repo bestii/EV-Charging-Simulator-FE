502cad856193e95f6c6d03038e7017286290a5f0
Bestin John <bestinjohn1993@gmail.com>
2025-03-23 19:18:17 +0100

    Enhance Footer component with improved layout and attribution for better visibility

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,20 @@
 const Footer = () => {
   return (
-    <footer className="container mx-auto text-center text-sm font-semibold">
-      <div className="border-t border-gray-950/5 py-6">
-        © {new Date().getFullYear()} EV Charging Simulator.
+    <footer className="container mx-auto text-sm font-semibold">
+      <div className="flex justify-between border-t border-gray-950/5 py-6">
+        <span>© {new Date().getFullYear()} EV Charging Simulator.</span>
+        <span>
+          <span className="me-1 text-gray-500">
+            {'Created / Code / Design by'}
+          </span>
+          <a
+            href="https://github.com/bestii"
+            className="underline"
+            target="_blank"
+          >
+            Bestin John
+          </a>
+        </span>
       </div>
     </footer>
   );
